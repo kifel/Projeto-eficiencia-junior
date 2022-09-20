685a9518acca23c93c3c468a515bf5d683a1ec65
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-19 22:34:28 -0300

    primeiros passos

---
+++ public/scripts/main.js
@@ -0,0 +1,81 @@
+window.addEventListener("scroll", onScroll);
+
+onScroll();
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  // verificar se a seção passou da linha
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  const sectionEndAt = sectionTop + sectionHeight;
+
+  const sectionEndPassedTargetLine = sectionEndAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    document.getElementById("navigation").classList.add("scroll");
+  } else {
+    document.getElementById("navigation").classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    document.getElementById("backToTopButton").classList.add("show");
+  } else {
+    document.getElementById("backToTopButton").classList.remove("show");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`#home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content,
+    #contact,
+    #contact header,
+    #contact .content`);
